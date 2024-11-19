a213c9978d80d75dd465f8c75e192601832fb4a2
Vikas Sijoriya <92936175+Sijoriya9981@users.noreply.github.com>
2024-11-20 02:05:01 +0530

    Update Course.jsx

--- BookStore_Frontend/src/components/Course.jsx
+++ BookStore_Frontend/src/components/Course.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Card from './Card';
+import Card from './Card.jsx';
 import { Link } from 'react-router-dom';
 import axios from 'axios'
 Link
