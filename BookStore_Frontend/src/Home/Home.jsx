bbad4cc6e272032f202f544337409a2df4933906
Vikas Sijoriya <92936175+Sijoriya9981@users.noreply.github.com>
2024-11-20 01:58:09 +0530

    Update Home.jsx

--- BookStore_Frontend/src/Home/Home.jsx
+++ BookStore_Frontend/src/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Banner from '../components/Banner';
 import Freebook from '../components/Freebook';
-import Footer from '../components/footer';
+import Footer from '../components/Footer';
 
 const Home = () => {
     return (
