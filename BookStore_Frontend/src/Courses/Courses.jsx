9ce19ba41de7d2e1904ecc134b41c784ac3cd53d
Vikas Sijoriya <92936175+Sijoriya9981@users.noreply.github.com>
2024-11-20 02:03:26 +0530

    Update Courses.jsx

--- BookStore_Frontend/src/Courses/Courses.jsx
+++ BookStore_Frontend/src/Courses/Courses.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import Course from '../components/course';
+import Navbar from '../components/Navbar.jsx';
+import Footer from '../components/Footer.jsx';
+import Course from '../components/course.jsx';
 
 const Courses = () => {
     return (
